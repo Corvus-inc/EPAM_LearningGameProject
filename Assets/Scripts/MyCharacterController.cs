b16fe260b8496342a167f7d644bce02dfdd6c8d7
Sergey <esa@ambidexter.io>
2021-09-17 05:16:02 +0300

    Primitive Character

---
+++ Assets/Scripts/MyCharacterController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCharacterController : MonoBehaviour
+{
+    [SerializeField] private Rigidbody playerRigidbody;
+
+    [Range(1, 10)]
+     public int mooveSpeed;
+
+    private Vector3 moveTo;
+
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerRigidbody.velocity = Vector3.forward * mooveSpeed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerRigidbody.velocity = -Vector3.right * mooveSpeed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerRigidbody.velocity = -Vector3.forward * mooveSpeed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            playerRigidbody.velocity = Vector3.right * mooveSpeed;
+        }
+    }
+}
