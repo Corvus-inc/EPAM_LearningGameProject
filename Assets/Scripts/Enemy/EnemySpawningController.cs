5f2014ed5a0d4f18f99122df4039c2061e4d88c9
Sergey <esa@ambidexter.io>
2021-09-27 12:23:04 +0300

    Mooving enemes to player

--- Assets/Scripts/Enemy/EnemySpawningController.cs
+++ Assets/Scripts/Enemy/EnemySpawningController.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class EnemySpawningController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private int enemyCounter;
+    [SerializeField] private float coolDown;
+    [SerializeField] private GameObject enemy;
+
     void Start()
     {
-        
+        StartCoroutine(DoCheck());
     }
 
-    // Update is called once per frame
-    void Update()
+    void CounterEnemyCheck()
     {
-        
+        if (enemyCounter > 0) { 
+            var inst = Instantiate(enemy, transform.position, Quaternion.identity, transform);
+            inst.transform.localPosition = new Vector3(Random.Range(-10, 10), 0f, Random.Range(-10, 10));
+
+            enemyCounter--;
+        }
     }
+
+    IEnumerator DoCheck()
+    {
+        for (; ; )
+        {
+            CounterEnemyCheck();
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+
 }
