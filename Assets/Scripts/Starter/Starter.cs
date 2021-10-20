529b16fb68d8965b405e6a6cca7696ed8ca0263f
SergeyE <Corvusincvr@gmail.com>
2021-10-20 22:33:15 +0300

    Added the stateSystem in the playerCharacter and  the Starter

--- Assets/Scripts/Starter/Starter.cs
+++ Assets/Scripts/Starter/Starter.cs
@@ -9,11 +9,14 @@ public class Starter : MonoBehaviour
     [SerializeField] private HealthBar _playerUIHealthBar;
     [SerializeField] private TMP_Text _playerIUClip;
 
+    private StatsSystem _playerStatsSystem;
     private HealthSystem _palyerHealthSystem;
 
     private void Awake()
     {
-        _palyerHealthSystem = new HealthSystem(_player.HealthPlayer);
+        _playerStatsSystem = new StatsSystem(_player.HealthPlayer, 0, _player.MooveSpeed, _player.timeBoostSpeed);
+        _player.SetStatsSystem(_playerStatsSystem);
+        _palyerHealthSystem = new HealthSystem(_playerStatsSystem.Health);
         _player.SetHealthSystem(_palyerHealthSystem);
         _playerUIHealthBar.Setup(_palyerHealthSystem);
         _playerUIHealthBar.SetColour(new Color32(33, 6, 102, 255));
