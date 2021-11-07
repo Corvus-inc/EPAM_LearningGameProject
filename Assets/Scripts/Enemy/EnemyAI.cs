80bae759166bd2188be6e182fdaea2ee84522285
SergeyE <Corvusincvr@gmail.com>
2021-11-07 14:39:11 +0300

    Added mutant on scene

--- Assets/Scripts/Enemy/EnemyAI.cs
+++ Assets/Scripts/Enemy/EnemyAI.cs
@@ -7,12 +7,13 @@ using Random = UnityEngine.Random;
 
 public class EnemyAI : MonoBehaviour
 {
-    [SerializeField] private Transform moveTargetPosition;
+    private Transform moveTargetPosition;
 
     private NavMeshAgent _navMeshAgent;
     private Vector3 _startingPosition;
     private Vector3 _roamPosition;
     private float _nextShootTime;
+    private float _scale;
     private State _state;
 
     private enum State
@@ -24,6 +25,7 @@ public class EnemyAI : MonoBehaviour
 
     private void Awake()
     {
+        _scale = gameObject.transform.localScale.x;
         moveTargetPosition = GameObject.Find("Player")?.transform;
         _navMeshAgent = GetComponent<NavMeshAgent>();
         _state = State.Roaming;
@@ -42,7 +44,7 @@ public class EnemyAI : MonoBehaviour
             default:
             case State.Roaming:
                 MoveTo(_roamPosition);
-                var reachedPositionDistance = 1f;
+                var reachedPositionDistance = 1f*_scale;
                 if (Vector3.Distance(transform.position, _roamPosition) < reachedPositionDistance)
                 {
                     _roamPosition = GetRoamingPosition();
@@ -52,19 +54,19 @@ public class EnemyAI : MonoBehaviour
                 break;
             case State.ChaseTarget:
                 MoveTo(moveTargetPosition.position);
-                float attackRange = 3f;
+                float attackRange = 3f*_scale;
                 if (Vector3.Distance(transform.position, moveTargetPosition.position) < attackRange)
                 {
                     if (Time.time > _nextShootTime)
                     {
                         MoveTo(transform.position);
                         Debug.Log("Attack");
-                        float fireRate = .1f;
+                        float fireRate = 1f;
                         _nextShootTime = Time.time + fireRate;
                     }
                 }
 
-                float stopChaseDistance = 8f;
+                float stopChaseDistance = 10f*_scale;
                 if (Vector3.Distance(transform.position, moveTargetPosition.position) > stopChaseDistance)
                 {
                     _state = State.GoingBackToStart;
@@ -72,7 +74,7 @@ public class EnemyAI : MonoBehaviour
                 break;
             case State.GoingBackToStart:
                 MoveTo(_startingPosition);
-                reachedPositionDistance = 1f;
+                reachedPositionDistance = 1f*_scale;
                 if (Vector3.Distance(transform.position, _startingPosition) < reachedPositionDistance)
                 {
                     _state = State.Roaming;
@@ -83,12 +85,12 @@ public class EnemyAI : MonoBehaviour
 
     private Vector3 GetRoamingPosition()
     {
-        return _startingPosition + GameUtils.Utils.GetRandomDir() * Random.Range(5f, 10f);
+        return _startingPosition + GameUtils.Utils.GetRandomDir() * Random.Range(20f, 30f);
     }
 
     private void FindTarget()
     {
-        float targetRange = 5f;
+        float targetRange = 10f*_scale;
         if (Vector3.Distance(transform.position, moveTargetPosition.position) < targetRange)
         {
             _state = State.ChaseTarget;
--- Assets/Scripts/Enemy/EnemyMutant.cs
+++ Assets/Scripts/Enemy/EnemyMutant.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMutant : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Range(0,1000)] [SerializeField] private int startHealthEnemy;
+    
+    [SerializeField] private LayerMask layerBullet;
+    [SerializeField] private HealthBarEnemy healthBarEnemy;
+    
+    private HealthSystem _healthSystem;
+
+    private void Awake()
     {
+        _healthSystem = new HealthSystem(startHealthEnemy);
+        _healthSystem.OnHealthStateMin += EnemyDie;
+        healthBarEnemy.HealthSystem = _healthSystem;
+        // healthBarEnemy.SetColour(Color.green);
+    }
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        bool check = GameUtils.Utils.CheckLayerMask(collision.gameObject, layerBullet);
         
+        if (check)
+        {
+            IBullet bullet = collision.gameObject.GetComponent<IBullet>();
+            int damage = bullet.GetBulletDamage();
+            _healthSystem.Damage(damage);
+            
+            StopCoroutine(bullet.DeactivatingBullet(0));
+            bullet.DeactivatingBullet();
+        }    
     }
 
-    // Update is called once per frame
-    void Update()
+    private void EnemyDie(object sender, EventArgs e)
     {
-        
-    }
+        _healthSystem.OnHealthStateMin -= EnemyDie;
+        Destroy(gameObject);    }
 }
